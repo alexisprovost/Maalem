7c2aeb83e1fef2f212cab7dd468ca7676a262429
Alec <alectufenkjian@gmail.com>
2021-03-11 20:18:53 -0500

    J'ai ajoutee Google Auth pour les connexions.

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+import Login from './Login';
+
+function App(){
+    return(
+        <Login />
+    )
+}
+
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component{
-
-    render(){
-        return(
-            <h1>Hello World</h1>
-        )
-    }
-}
-
+import App from './App';
 
 ReactDOM.render(
     <App />,
