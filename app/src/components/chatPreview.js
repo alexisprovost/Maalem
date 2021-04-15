812978f17667e66ed629fff3f75f70fed9cf29bc
Alexis <provost21j@gmail.com>
2021-04-15 15:52:13 -0400

    fix css chat img

--- app/src/components/chatPreview.js
+++ app/src/components/chatPreview.js
@@ -10,7 +10,12 @@ export default function Chat() {
             Receiver Name
         </span>
         <div className="userPic">
-            <span style={{"background-image": 'url("https://picsum.photos/50/50")'}}></span>
+            <span style={{'background-image': 'url(https://picsum.photos/50/50)',
+            width: '50px',
+            height: '50px',
+            display: 'block',
+            'background-size': '100% auto',
+            'border-radius': '50%'}}></span>
   
         </div>
         </>
