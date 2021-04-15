713159db1ff88669e17467fa472ec255b8413a2d
Alexis <provost21j@gmail.com>
2021-04-15 16:06:52 -0400

    chat

--- app/src/components/chatPreview.js
+++ app/src/components/chatPreview.js
@@ -1,22 +1,33 @@
 import react from 'react';
 import ReactDOM, { render } from 'react-dom';
 import "./style.css";
-import "./userPic.jpg";
 
 export default function Chat() {
     return (
         <>
-        <span className="receiverName">
-            Receiver Name
-        </span>
-        <div className="userPic">
-            <span style={{'background-image': 'url(https://picsum.photos/50/50)',
-            width: '50px',
-            height: '50px',
-            display: 'block',
-            'background-size': '100% auto',
-            'border-radius': '50%'}}></span>
-  
+        <div className="contactHolder">
+            <span className="receiverName">
+                Receiver Name
+            </span>
+            <div className="userPic">
+                <span style={{'background-image': 'url(https://picsum.photos/50/50)',
+                width: '50px',
+                height: '50px',
+                display: 'block',
+                'background-size': '100% auto',
+                'border-radius': '50%'}}></span>
+            </div>
+        </div>
+        <div className="messagesHolder">
+            <div className="messageFrom">
+                test
+            </div>
+            <div className="messageTo">
+                test
+            </div>
+        </div>
+        <div className="chatInput">
+            
         </div>
         </>
     );   
