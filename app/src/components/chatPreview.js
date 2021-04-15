8ac6832cd5b77b2fa9c3720e16c4403077d1dff6
BoltyZeus <74887519+BoltyZeus@users.noreply.github.com>
2021-04-15 15:45:28 -0400

    Chat

--- app/src/components/chatPreview.js
+++ app/src/components/chatPreview.js
@@ -1,10 +1,18 @@
 import react from 'react';
 import ReactDOM, { render } from 'react-dom';
+import "./style.css";
+import "./userPic.jpg";
 
 export default function Chat() {
     return (
         <>
-        chat field
+        <span className="receiverName">
+            Receiver Name
+        </span>
+        <div className="userPic">
+            <span style={{"background-image": 'url("https://picsum.photos/50/50")'}}></span>
+  
+        </div>
         </>
     );   
 }
