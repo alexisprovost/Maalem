0996f3d971e49084e212ac6431f3b6cb0bf0b498
Alexis <provost21j@gmail.com>
2021-04-22 14:17:32 -0400

    added cards in api

---
+++ api/models/card.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create schema & model
+const Cardschema = new Schema({
+    title: {
+        type: String
+    },
+    subject: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    comments: [{
+        url: String
+    }],
+    author: {
+        type: String
+    },
+    reward: {
+        type: Number
+    }
+});
+
+const Card = mongoose.model('card', Cardschema);
+module.exports = Card;
