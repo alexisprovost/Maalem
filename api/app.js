6cfaaf7868ce7117e12e2a5c78635099450eb651
Alexis Provost <41759290+alexisprovost@users.noreply.github.com>
2021-05-17 13:12:18 -0400

    fix cors

--- api/app.js
+++ api/app.js
@@ -24,8 +24,7 @@ const io = socketio(server, {
     methods: [],
     allowedHeaders: [],
     exposedHeaders: [],
-    preflightContinue: true,
-    credentials:true
+    preflightContinue: true
   }
 });
 
@@ -41,10 +40,8 @@ var corsOptions = {
 require('./initDB')();
 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Origin', ['http://localhost:3000']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  res.append('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
